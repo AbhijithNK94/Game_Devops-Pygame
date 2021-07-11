f3172975c7e1b99da0cdfbdec5ecc5b320105b70
Abhijith Nandakumar <abhijithnandakumar5@gmail.com>
2021-07-11 09:37:31 +0530

    Update Rock_paper_scissors_game.py

--- Rock_paper_scissors_game.py
+++ Rock_paper_scissors_game.py
@@ -1,5 +1,5 @@
 """
-This is a two-player Rock-Paper-Scissors game. (Hint: Ask for player
+This is a simple two-player Rock-Paper-Scissors game. (Hint: Ask for player
 plays (using input), compare them, print out a message of congratulations
 to the winner, and ask if the players want to start a new game)
 Remember the rules:
